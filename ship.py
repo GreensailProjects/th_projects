fab96116d630f4545d5551a2ce08833dcea6ea42
GreensailProjects <147617207+GreensailProjects@users.noreply.github.com>
2023-11-02 16:41:31 +0300

    Update ship.py

--- ship.py
+++ ship.py
@@ -3,7 +3,7 @@ class Ship:
     def __init__(self, ai_game):
         self.screen=ai_game.screen
         self.screen_rect=ai_game.screen.get_rect()
-        self.image =pygame.image.load('images/d7ee96a96b8e1b694af3a1f0f6238f43.bmp')
+        self.image =pygame.image.load('images/ship.bmp')
         self.rect = self.image.get_rect()
         self.rect.midbottom=self.screen_rect.midbottom
         self.moving_right = False
